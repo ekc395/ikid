4ba24fb6e13df0cb7bee0bd3cadf789a2a511ba3
ekc395 <ekc395@gmail.com>
2026-02-04 18:12:13 -0800

    Restarting with Playground

---
+++ ikid/ikid/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ikid
+//
+//  Created by Ethan Chen on 2/4/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
