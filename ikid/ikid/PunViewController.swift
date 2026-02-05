15a032b5eb550eb10c55b1bff23902e2550138bd
ekc395 <ekc395@gmail.com>
2026-02-04 20:24:13 -0800

    First view controllers and tab bar

---
+++ ikid/ikid/PunViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PunViewController.swift
+//  ikid
+//
+//  Created by Ethan Chen on 2/4/26.
+//
+
+import UIKit
+
+class PunViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
